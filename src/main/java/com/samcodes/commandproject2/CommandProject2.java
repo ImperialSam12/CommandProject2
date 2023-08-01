8f36541ae504833ff49d20454c5645a1c6c6818e
SamHernandez <samdamangaming@gmail.com>
2023-08-01 08:46:06 -0700

    Refactors code, optimizes commits, adds /echo command.

--- src/main/java/com/samcodes/commandproject2/CommandProject2.java
+++ src/main/java/com/samcodes/commandproject2/CommandProject2.java
@@ -1,13 +1,10 @@
 package com.samcodes.commandproject2;
 
-import com.samcodes.commands.Commands;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
+import com.samcodes.commandproject2.commands.EchoCommand;
+import com.samcodes.commandproject2.commands.FeedCommand;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.Objects;
 
 public class CommandProject2 extends JavaPlugin {
@@ -16,7 +13,8 @@ public class CommandProject2 extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "CommandProject2 is enabled!");
-        Objects.requireNonNull(getCommand("feed")).setExecutor(new Commands());
+        Objects.requireNonNull(getCommand("feed")).setExecutor(new FeedCommand());
+        Objects.requireNonNull(getCommand("echo")).setExecutor(new EchoCommand());
     }
 
     @Override
